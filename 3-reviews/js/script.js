9a8d2e37c1a9b0ebc090b37347664543301cfc1b
KylianCrv <kylian.crv@hotmail.com>
2022-09-19 14:15:53 +0200

    tp 3 done

---
+++ 3-reviews/js/script.js
@@ -0,0 +1,71 @@
+const persons = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+const previousBtn = document.querySelector('#previous-button')
+const nextBtn = document.querySelector('#next-button')
+
+const display = document.querySelector('#response');
+let count = 0;
+
+    showDisplay(count)
+
+nextBtn.addEventListener('click',function(){
+    if(count<persons.length-1){
+        count++;
+        showDisplay(count)
+    }else
+    count = 0;
+    showDisplay(count)
+})
+
+previousBtn.addEventListener('click',function(){
+    if(count>0){
+        count--;
+        showDisplay(count)}
+    else
+    count = persons.length-1
+    showDisplay(count)
+})
+
+function showDisplay(count){
+    display.innerHTML = `id : ${persons[count].id} <br>
+    name : ${persons[count].name} <br>
+    job : ${persons[count].job}<br>
+    img : <img src="${persons[count].img}"></img> <br>
+    text : ${persons[count].text}`;
+}
